8f42bbd7d256c3e90616e9adc4bdd4d599dbf02d
brent <liuxuzhong@ymyai.com>
2024-09-02 20:33:52 +0800

    add async demo

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ npm install aonweb --save or npm install aonweb@2.0.2 --save
 
 ## Getting Started
 
+### synchronous call
+
 Please follow the [installation](#installation) instruction and execute the following JS code:
 
 ```javascript
@@ -61,8 +63,93 @@ prediction();
 
 ```
 
+### asynchronous call
+
+Please follow the [installation](#installation) instruction and execute the following JS code:
+
+```javascript
+
+import { AI, User } from 'aonweb'
+
+const prediction_async = async () => {
+  let user = new User()
+  let login_user = await user.islogin()
+  if (!login_user) {
+      login_user = await user.login()
+      if (!login_user) {
+          console.log("login failed,please try again later");
+          return
+      }
+  }
+
+  const options = {
+    //Please replace with your own app_key.
+    app_key: REPLACE_APP_KEY,
+    is_async: true,
+  }
+  const aonet = new AI(options)
+
+  let params = {}
+  params['meta-llama-3-8b-instruct@iAON-internal'] = {
+    "top_p": 0.95,
+    "prompt": "Johnny has 8 billion parameters. His friend Tommy has 70 billion parameters. What does this mean when it comes to speed?",
+    "temperature": 0.7,
+    "system_prompt": "You are a helpful assistant",
+    "length_penalty": 1,
+    "max_new_tokens": 512,
+    "stop_sequences": "<|end_of_text|>,<|eot_id|>",
+    "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+    "presence_penalty": 0
+  }
+  let res = await aonet.prediction(['meta-llama-3-8b-instruct@iAON-internal'], { ...params })
+  if (res.code === 200 && res.data) {
+      let taskId = res.data.task_id
+      let timing = setInterval(async () => {
+          try {
+              let res = await aonet.task(taskId)
+              console.log(res)
+              if (res.code === 200 && res.data) {
+                  res = res.data
+                  console.log('res 111 =',res)
+                  if (res.status !== 0) {
+                    clearInterval(timing)
+                  }
+                  if (res.status == 3 || res.status == 4) {
+                    let msg = (res.error && res.error) || 'Operation timed out'
+                    console.log('res = ',res)
+                  }
+
+                  if (res.status == 2 || res.status == 1) {
+                    console.log('create successfully, result:',res.result)
+                    return_text.value = res.result.join("").trim();
+			        console.log("return_text = ", return_text)
+                  }
+
+              }
+              if (res.code === 201) {
+                  clearInterval(timing)
+                  console.log('res = ',res)
+              }
+          } catch (error) {
+              console.log(error)
+              if (timing) {
+                  clearInterval(timing)
+              }
+          }
+      }, 1000)
+  }
+  if (res.code === 201) {
+      console.log('res = ',res)
+  }
+}
+
+prediction_async();
+
+```
+
 ### Constructor
 
-name | type | Description
+name | type | Description | default
 ------------ | ------------- | -------------
-*app_key* | string | **Required**.
+*app_key* | string | **Required**. |
+*is_async* | bool |  **Options**. | false
--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,83 @@ import { ref } from 'vue'
 const return_text = ref('')
 const showLoading = ref(false);
 
+const prediction_async = async () => {
+  showLoading.value = true
+  let user = new User()
+  let login_user = await user.islogin()
+  if (!login_user) {
+      login_user = await user.login()
+      if (!login_user) {
+          console.log("login failed,please try again later");
+          showLoading.value = false
+          return
+      }
+  }
+
+  const options = {
+    //Please replace with your own app_key.
+    app_key: "20098a6c-19af-47b8-8b65-9d74652e587c",
+    is_async: true,
+  }
+  const aonet = new AI(options)
+
+  let params = {}
+  params['meta-llama-3-8b-instruct@iAON-internal'] = {
+    "top_p": 0.95,
+    "prompt": "Johnny has 8 billion parameters. His friend Tommy has 70 billion parameters. What does this mean when it comes to speed?",
+    "temperature": 0.7,
+    "system_prompt": "You are a helpful assistant",
+    "length_penalty": 1,
+    "max_new_tokens": 512,
+    "stop_sequences": "<|end_of_text|>,<|eot_id|>",
+    "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+    "presence_penalty": 0
+  }
+  let res = await aonet.prediction(['meta-llama-3-8b-instruct@iAON-internal'], { ...params })
+  if (res.code === 200 && res.data) {
+      let taskId = res.data.task_id
+      let timing = setInterval(async () => {
+          try {
+              let res = await aonet.task(taskId)
+              console.log(res)
+              if (res.code === 200 && res.data) {
+                  res = res.data
+                  console.log('res 111 =',res)
+                  if (res.status !== 0) {
+                    showLoading.value = false
+                    clearInterval(timing)
+                  }
+                  if (res.status == 3 || res.status == 4) {
+                      let msg = (res.error && res.error) || 'Operation timed out'
+                      console.log('res = ',res)
+                  }
+
+                  if (res.status == 2 || res.status == 1) {
+                      console.log('create successfully, result:',res.result)
+                      return_text.value = res.result.join("").trim();
+			                console.log("return_text = ", return_text)
+                  }
+
+              }
+              if (res.code === 201) {
+                  clearInterval(timing)
+                  console.log('res = ',res)
+              }
+          } catch (error) {
+            showLoading.value = false
+              console.log(error)
+              if (timing) {
+                  clearInterval(timing)
+              }
+          }
+      }, 1000)
+  }
+  if (res.code === 201) {
+    showLoading.value = false
+    console.log('res = ',res)
+  }
+}
+
 const prediction = async () => {
   try {
     showLoading.value = true
@@ -55,7 +132,10 @@ const prediction = async () => {
 		<!-- 页面内容 -->
 		<div class="">
       <button class="" @click="prediction">
-        <text>生成</text>
+        <text>生成-同步</text>
+      </button>
+      <button class="" @click="prediction_async">
+        <text>生成-异步</text>
       </button>
 			<div class="uni-form-item uni-column">
         <div class="res_img">{{return_text}}</div> 
